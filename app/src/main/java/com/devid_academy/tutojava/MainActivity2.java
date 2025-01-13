c753b15ee2dc64fe98d7e7aeb5fa282551fedfe9
thierryleung <ahkhiat@hotmail.com>
2025-01-13 16:43:16 +0100

    landscape views

--- app/src/main/java/com/devid_academy/tutojava/MainActivity2.java
+++ app/src/main/java/com/devid_academy/tutojava/MainActivity2.java
@@ -1,12 +1,15 @@
 package com.devid_academy.tutojava;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -30,12 +33,13 @@ public class MainActivity2 extends AppCompatActivity {
         User user = new User();
 
         if(getIntent().getExtras() != null) {
-            user = (User) getIntent().getSerializableExtra(MainActivity.KEY_USER);
+            user = (User) getIntent().getParcelableExtra(MainActivity.KEY_USER);
         }
 
         EditText etFirstname = findViewById(R.id.et_main2);
 
         etFirstname.setText(user.getFirstname(), EditText.BufferType.EDITABLE );
+        Toast.makeText(this, "Les données ont été récuperés dans un Object avec Parcelable", Toast.LENGTH_SHORT).show();
 
         btnMain2.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/devid_academy/tutojava/User.java
+++ app/src/main/java/com/devid_academy/tutojava/User.java
@@ -1,8 +1,13 @@
 package com.devid_academy.tutojava;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
 import java.io.Serializable;
 
-public class User implements Serializable {
+public class User implements Parcelable {
     private String firstname;
     private String lastname;
     private int age;
@@ -15,6 +20,36 @@ public class User implements Serializable {
 
     public User(){}
 
+    protected User(Parcel in) {
+        firstname = in.readString();
+        lastname = in.readString();
+        age = in.readInt();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        parcel.writeString(firstname);
+        parcel.writeString(lastname);
+        parcel.writeInt(age);
+    }
+
+
+// Getters @ Setters
     public String getFirstname() {
         return firstname;
     }
@@ -33,5 +68,4 @@ public class User implements Serializable {
     public void setAge(int age) {
         this.age = age;
     }
-
 }
